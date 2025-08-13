2f8487f11f667b3316bc957a2329363d3e93f5be
garchomp-game <otoka.japanesedrum@gmail.com>
2025-08-13 14:53:11 +0900

    style: order imports

--- backend-laravel/app/Providers/AuthServiceProvider.php
+++ backend-laravel/app/Providers/AuthServiceProvider.php
@@ -2,10 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 use App\Models\Post;
 use App\Policies\PostPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
